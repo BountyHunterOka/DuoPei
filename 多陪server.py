4dd18c89f71311f4c0085c94fba5a4f1b389f33e
BountyHunterOka <434291091spd@gmail.com>
2025-09-28 17:36:05 +0800

    Update 多陪server.py

--- 多陪server.py
+++ 多陪server.py
@@ -138,7 +138,7 @@ def extract_order_id(decrypted_json_str):
         data = json.loads(decrypted_json_str)
         order_list = data.get("list", [])
         
-        sensitive_words = ['腿','胸','不续单','狱卒','玉足','欲姐','萝莉','广东','四川','新客首单体验','10.0']
+        sensitive_words = ['腿','胸','不续单','狱卒','玉足','欲姐','萝莉','广东','四川']
 
         for order in order_list:
             memo = order.get("userMemo", "")
