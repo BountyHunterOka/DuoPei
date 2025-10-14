3ca2e964d75b9b066b212faa4bbd43e9b14d2a22
BountyHunterOka <434291091spd@gmail.com>
2025-10-14 16:55:49 +0800

    Update 多陪server.py

--- 多陪server.py
+++ 多陪server.py
@@ -1,6 +1,6 @@
 from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 import threading
 import time
 import random
@@ -10,6 +10,7 @@ import requests
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 from datetime import datetime, timezone, timedelta
+
 # import os
 # import platform
 tz = timezone(timedelta(hours=8))
@@ -17,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -36,30 +37,40 @@ def stop_book():
     stop_grabbing()
     return "结束抢单"
 
+
 @app.get("/voice_start")
 def start_voice():
     start_talking()
     return "过滤连麦单"
+
 
 @app.get("/voice_stop")
 def stop_voice():
     stop_talking()
     return "不过滤连麦单"
 
+
 @app.get("/check_running")
 def check():
     if running:
-     return "运行中"
+        return "运行中"
     else:
-     return "已暂停"
+        return "已暂停"
+
 
 @app.get("/check_talking")
 def check():
     if voice_talking:
-     return "过滤连麦中"
+        return "过滤连麦中"
     else:
-     return "未过滤连麦"
+        return "未过滤连麦"
 
+@app.post("/receive")
+async def receive_data(request: Request):
+    raw = await request.body()  # 原始字节
+    text = raw.decode("utf-8")  # 转成字符串
+    print("[收到文本]", text)
+    return {"status": "ok", "received": text}
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Union[str, None] = None):
 #     return {"item_id": item_id, "q": q}
@@ -82,16 +93,16 @@ KEY_HEX = "81b120ef00216c33b266763abb02e6d1"
 IV_HEX = "e6a4cc0507dfe344b042289eeb945dce"
 
 HEADERS = {
-    "platform":"app",
-    "Accept":"*/*",
-    "Accept-Encoding":"gzip, deflate, br",
-    "Content-Type":"application/x-www-form-urlencoded",
-    "authorization-token":"cf990524993944c4874248585d453276",
-    "sid":"47",
-    "Host":"api.duopei.feiniaowangluo.com",
-    "User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 18 5 like Mac OS x)AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Html5Plus/1.0 (lmmersed/20) uni-app",
-    "Accept-Language":"en-GB,en;q=0.9",
-    "Connection":"keep-alive"
+    "platform": "app",
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "authorization-token": "cf990524993944c4874248585d453276",
+    "sid": "47",
+    "Host": "api.duopei.feiniaowangluo.com",
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18 5 like Mac OS x)AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Html5Plus/1.0 (lmmersed/20) uni-app",
+    "Accept-Language": "en-GB,en;q=0.9",
+    "Connection": "keep-alive"
 }
 
 BASE_URL = "https://api.duopei.feiniaowangluo.com"
@@ -101,9 +112,11 @@ session.headers.update(HEADERS)
 running = False
 voice_talking = True
 
+
 # ========== 日志输出 ==========
 def log(text):
     print(text)
+
 
 # ========== AES 解密 ==========
 def decrypt_aes_cbc(encrypted_b64, key_hex, iv_hex):
@@ -117,6 +130,7 @@ def decrypt_aes_cbc(encrypted_b64, key_hex, iv_hex):
     except Exception as e:
         log(f"[解密失败] {e}")
         return None
+
 
 # ========== 刷新订单列表 ==========
 def refresh_list():
@@ -133,14 +147,14 @@ def refresh_list():
         log(f"[刷新失败] {e}")
         return None
 
+
 # ========== 提取订单 ID ==========
 def extract_order_id(decrypted_json_str):
     try:
-        print(decrypted_json_str)
         data = json.loads(decrypted_json_str)
         order_list = data.get("list", [])
-        
-        sensitive_words = ['腿','胸','不续单','狱卒','玉足','欲姐','萝莉','广东','四川']
+
+        sensitive_words = ['腿', '胸', '不续单', '狱卒', '玉足', '欲姐', '萝莉', '广东', '四川']
 
         # for order in order_list:
         #     memo = order.get("userMemo", "")
@@ -153,13 +167,14 @@ def extract_order_id(decrypted_json_str):
                 log("[跳过订单] 有备注")
                 continue
             names = order.get("item", {}).get("names", [])
-            if voice_talking and any(keyword in name for keyword in ['连麦','听歌'] for name in names):
+            if voice_talking and any(keyword in name for keyword in ['连麦', '听歌'] for name in names):
                 log("[跳过订单] 不要连麦单")
                 continue
             return order.get("id")
     except Exception as e:
         log(f"[提取订单 ID 失败] {e}")
     return None
+
 
 def extract_ts(decrypted_json_str):
     try:
@@ -181,30 +196,31 @@ def sleep_time(create_ts, wait_time):
     if sleep_seconds < 0:
         random_sleep = random.uniform(1, 2.5)
     else:
-        random_sleep = random.uniform(sleep_seconds, sleep_seconds+2.25)
+        random_sleep = random.uniform(sleep_seconds, sleep_seconds + 2.25)
     print(sleep_seconds)
     time.sleep(random_sleep)
-    
+
+
 # ========== 抢单 ==========
-def confirm_order(order_id, create_ts):
-    url = f"{BASE_URL}/s/c/order/confirm"
-    data = {"id": order_id}
-    try:
-        sleep_time(create_ts, 15.5)
-        while running:
-            resp = session.post(url, data=data, timeout=3.5)
-            da = resp.json()
-            confirm_rep = decrypt_aes_cbc(da["response"], KEY_HEX, IV_HEX)
-            if not confirm_rep:
-                break
-            log(f"[抢单结果] {confirm_rep}")
-            # if '未满足' in confirm_rep:
-            #     log("等待中...继续尝试")
-            #     time.sleep(9.5)
-            #     continue
-            break
-    except Exception as e:
-        log(f"[抢单失败] {e}")
+# def confirm_order(order_id, create_ts):
+#     url = f"{BASE_URL}/s/c/order/confirm"
+#     data = {"id": order_id}
+#     try:
+#         sleep_time(create_ts, 15.5)
+#         while running:
+#             resp = session.post(url, data=data, timeout=3.5)
+#             da = resp.json()
+#             confirm_rep = decrypt_aes_cbc(da["response"], KEY_HEX, IV_HEX)
+#             if not confirm_rep:
+#                 break
+#             log(f"[抢单结果] {confirm_rep}")
+#             # if '未满足' in confirm_rep:
+#             #     log("等待中...继续尝试")
+#             #     time.sleep(9.5)
+#             #     continue
+#             break
+#     except Exception as e:
+#         log(f"[抢单失败] {e}")
 
 
 # ========== 主循环 ==========
@@ -217,15 +233,18 @@ def run_loop(interval):
         if decrypted:
             order_id = extract_order_id(decrypted)
             if order_id:
-                create_ts = extract_ts(decrypted)
-                log(f"[发现订单] ID = {order_id}")
-                confirm_order(order_id, create_ts)
+                print('有效订单')
+                print(decrypted)
+                # create_ts = extract_ts(decrypted)
+                # log(f"[发现订单] ID = {order_id}")
+                # confirm_order(order_id, create_ts)
                 # play_sound()
             else:
                 log("[无新订单]")
         else:
             log("[解密失败或网络异常]")
         time.sleep(random.uniform(2, 3.5))
+
 
 # ========== 控制函数 ==========
 def start_grabbing():
@@ -241,6 +260,7 @@ def start_grabbing():
     running = True
     log("[启动抢单]")
     threading.Thread(target=run_loop, args=(interval,), daemon=True).start()
+
 
 def stop_grabbing():
     global running
@@ -258,4 +278,3 @@ def stop_talking():
     global voice_talking
     voice_talking = False
     log("[不过滤连麦单.]")
-
